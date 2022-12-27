f90f50625da7f18ff05eba501de9767d1bb33cc4
afn <hey@afn.lol>
2022-12-27 16:30:35 -0500

    feat: progress indicators

---
+++ src/lib/ProgressIndicator/ProgressIndicator.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+	export let value: number | undefined = 0;
+	export let max = 100;
+</script>
+
+<!-- It's impossible to animate the value of a progress element as far as I know -->
+<svelte:element
+	this={!value ? 'div' : 'progress'}
+	role="progressbar"
+	class="progress"
+	{value}
+	{max}
+>
+	<!-- Indeterminate progress value -->
+	{#if !value}
+		<span class="progress-value" />
+		<!-- Second progress value for better animation -->
+		<span class="progress-value-2" />
+	{/if}
+</svelte:element>
+
+<style lang="scss">
+	.progress {
+		-webkit-appearance: none;
+		-moz-appearance: none;
+		appearance: none;
+		border: 0;
+		height: 4px;
+		border-radius: 0;
+		width: auto;
+		position: relative;
+		background-color: var(--md-sys-color-surface-variant);
+	}
+
+	progress {
+		display: block;
+		&::-webkit-progress-bar {
+			background-color: var(--md-sys-color-surface-variant);
+		}
+
+		&[value]::-webkit-progress-value {
+			background-color: var(--md-sys-color-primary);
+		}
+
+		&[value]::-moz-progress-bar {
+			background-color: var(--md-sys-color-primary);
+		}
+	}
+
+	// styling for indeterminate value
+	.progress-value,
+	.progress-value-2 {
+		display: inline-block;
+		position: absolute;
+		height: 4px;
+		background-color: var(--md-sys-color-primary);
+	}
+
+	.progress-value {
+		animation: inditermate 3s var(--md-sys-motion-easing-standard) 0.5s infinite;
+	}
+
+	.progress-value-2 {
+		animation: inditermate 3s var(--md-sys-motion-easing-standard) 1s infinite;
+	}
+
+	@keyframes inditermate {
+		0% {
+			width: 0;
+			margin-left: 0;
+		}
+		50% {
+			width: 100%;
+			margin-left: 0;
+		}
+		100% {
+			width: 0;
+			margin-left: 100%;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,24 @@
 <script>
-	import Switch from '$lib/Switch/Switch.svelte';
+	import ProgressIndicator from '$lib/ProgressIndicator/ProgressIndicator.svelte';
+import Switch from '$lib/Switch/Switch.svelte';
 </script>
 
 <main class="wrapper">
 	<h1>Material 3 Svelte</h1>
 	<p>Svelte UI component library for Google's Material 3.</p>
 
-	<div class="label-large">Switch</div>
-	<Switch />
+	<div>
+	    <div class="label-large">Switch</div>
+    	<Switch />
+	</div>
+
+    <br/>
+    <div>
+        <div class="label-large">Progress Indicator</div>
+        <ProgressIndicator/>
+        <br/>
+        <ProgressIndicator value={29}/>
+    </div>
 </main>
 
 <style>
