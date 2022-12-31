f5fe5a3837e0d726916d16fd071e8d8b92cd61dc
afn <hey@afn.lol>
2022-12-31 15:50:11 -0500

    feat(TextInput): outlined version

--- src/lib/TextInput/TextInput.svelte
+++ src/lib/TextInput/TextInput.svelte
@@ -1,34 +1,16 @@
 <script lang="ts">
 	export let placeholder = 'hiii';
+	export let type = 'filled';
 	let hasText: string;
 </script>
 
 <div class="text-input-container">
-	<input type="text" class:hasText bind:value={hasText} />
+	<input class="input-{type}" type="text" class:hasText bind:value={hasText} />
 	<span class="placeholder">{placeholder}</span>
 </div>
 
 <style lang="scss">
 	@import '../../styles/typography.module.scss';
-
-	input {
-		@include body-large;
-		height: 40px;
-		padding-top: 14px;
-		padding-left: 16px;
-		padding-right: 16px;
-		border-radius: 4px 4px 0 0;
-		border: none;
-		color: var(--md-sys-color-on-surface);
-		background-color: var(--md-sys-color-surface-variant);
-		border-bottom: 1px solid var(--md-sys-color-on-surface-variant);
-	}
-
-	input::placeholder {
-		display: none;
-		opacity: 0;
-		visibility: hidden;
-	}
 
 	.text-input-container {
 		position: relative;
@@ -36,32 +18,87 @@
 		height: 56px;
 	}
 
-	.placeholder {
-		@include body-large;
-		color: var(--md-sys-color-on-surface-variant);
-		transition: all 0.2s var(--md-sys-motion-easing-standard);
-		background-color: var(--md-sys-color-surface-variant);
-		position: absolute;
-		left: 16px;
-		top: 18px;
+	// impossible to animate default placeholder without a bunch of js
+	input::placeholder {
+		display: none;
+		opacity: 0;
+		visibility: hidden;
 	}
 
-	input:focus {
-		opacity: 1;
-		outline: none;
-		border-bottom-color: var(--md-sys-color-primary);
+	.placeholder {
+		@include body-large;
+		transition: all 0.2s var(--md-sys-motion-easing-standard);
+		position: absolute;
+		left: 18px;
+		top: 18px;
+		color: var(--md-sys-color-on-surface-variant);
+		pointer-events: none;
+		user-select: none;
+	}
 
-		+ .placeholder {
-			@include body-small;
-			opacity: 1;
-			visibility: visible;
-			color: var(--md-sys-color-primary);
-			transform: translateY(-10px);
+	input {
+		@include body-large;
+		height: 40px;
+		transition: all 0.2s var(--md-sys-motion-easing-standard);
+		box-sizing: border-box;
+		height: 56px;
+		vertical-align: top;
+	}
+
+	.input-filled {
+		border-radius: 4px 4px 0 0;
+		border: none;
+		color: var(--md-sys-color-on-surface);
+		background-color: var(--md-sys-color-surface-variant);
+		border-bottom: 1px solid var(--md-sys-color-on-surface-variant);
+		padding: 16px 16px 2px 16px;
+
+		&:focus {
+			outline: none;
+			padding: 16px 16px 1px 16px;
+			border-bottom: 2px solid var(--md-sys-color-primary);
+
+			+ .placeholder {
+				@include body-small;
+				color: var(--md-sys-color-primary);
+				transform: translateY(-10px);
+			}
 		}
 	}
 
-	.hasText + .placeholder {
-		@include body-small;
-		transform: translateY(-10px);
+	.input-outlined {
+		border-radius: 4px;
+		color: var(--md-sys-color-on-surface);
+		background-color: transparent;
+		border: 1px solid var(--md-sys-color-outline);
+		padding: 8px 16px;
+
+		+ .placeholder {
+			padding: 0 4px;
+			background-color: var(--md-sys-color-background);
+		}
+
+		&:focus {
+			outline: none;
+			border-color: var(--md-sys-color-primary);
+
+			+ .placeholder {
+				@include body-small;
+				color: var(--md-sys-color-primary);
+				transform: translateY(-24px);
+			}
+		}
+	}
+
+	.hasText {
+		&.input-filled + .placeholder {
+			@include body-small;
+			transform: translateY(-10px);
+		}
+
+		&.input-outlined + .placeholder {
+			@include body-small;
+			transform: translateY(-24px);
+		}
 	}
 </style>
